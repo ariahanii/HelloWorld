fac2929e309850b53f42700e97bf331b36c39b5d
Aria Hani Tabaei <aria.hani.t@gmail.com>
2023-09-17 12:57:56 +0300

    adding the version that takes input

--- inputless.py
+++ inputless.py
@@ -1,19 +1,12 @@
 from time import sleep
 
-result = input('What do you want to write?\n').lower()
 letters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#creating empty space for the word
-# It's also appealing visually
-word = list('')
-for i in result:
-    word.append('#')
-
-
-result = list(result)
+word = list('###########')
+result = list("hello world")
 
 # 11 is the number of letters in hello world
-for i in range(len(result)):
+for i in range(11):
     while True:
         if word[i] == result[i]:
             break
