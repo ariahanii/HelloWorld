f03985ace65f5e60c90aa651eba213fe4e1582a0
Aria Hani Tabaei <aria.hani.t@gmail.com>
2023-09-17 12:59:44 +0300

    solving the bugs

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 result = input('What do you want to write?\n').lower()
-letters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letters = [',', '!', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 #creating empty space for the word
 # It's also appealing visually
